0d0a1c076ce2756f4b0c04a3bde7a28b5bb41319
aleynak-git <kofaleyna@gmail.com>
2021-03-07 14:10:26 +0300

    ui is completed

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from tkinter import *
 
 # ---------------- Fonksiyon Bölümü
 def center_window(w=300, h=300):
-    w = window.winfo_reqwidth()
-    h = window.winfo_reqheight()
     ws = window.winfo_screenwidth()
     hs = window.winfo_screenheight()
     x = (ws / 2) - (w / 2)
@@ -16,8 +14,8 @@ def center_window(w=300, h=300):
 
 
 window = Tk()
-window.title("Password Manager")
-window.config(padx=50, pady=50)
+window.title("Password Manager-v1.0")
+window.config(padx=20, pady=20)
 
 canvas = Canvas(width=200, height=200)
 logo_img = PhotoImage(file="lock-2.png")
@@ -29,12 +27,32 @@ canvas.grid(row=0, column=1)
 
 # labels
 website_label = Label(text="Website: ")
-website_label.grid(row=1, column=0)
+website_label.grid(row=1, column=0, sticky=E)
 email_label = Label(text="E-mail/Username: ")
-email_label.grid(row=2, column=0)
+email_label.grid(row=2, column=0, sticky=E)
 password_label = Label(text="Password: ")
 password_label.grid(row=3, column=0)
 
+# Entries
+website_text = StringVar()
+website_entry = Entry(width=52, textvariable=website_text)
+website_entry.focus()
+website_entry.grid(row=1, column=1, columnspan=2)
 
-center_window(500, 500)
+email_text = StringVar()
+email_text.set("aleynakof@gmail.com")
+email_entry = Entry(width=52, textvariable=email_text)
+email_entry.grid(row=2, column=1, columnspan=2)
+
+password_text = StringVar()
+password_entry = Entry(width=33, textvariable=password_text, show="*")
+password_entry.grid(row=3, column=1)
+
+# buttons
+generate_password_button = Button(text="generate password")
+generate_password_button.grid(row=3, column=2)
+add_button = Button(text="Add", width=44)
+add_button.grid(row=4, column=1, columnspan=2)
+
+center_window(500, 400)
 window.mainloop()
